d51db5dfdff28d5e2a496a39e9447bc7263ff274
mortent <morten.j.torkildsen@gmail.com>
2013-07-25 17:49:14 -0400

    Simplified tests

--- test/algorithms/sort/test_insertion_sort.rb
+++ test/algorithms/sort/test_insertion_sort.rb
@@ -1,16 +1,17 @@
 require 'test/unit'
 require_relative '../../../lib/algorithms/sort/sort'
+include Algorithms
 
 class TestInsertionSort < Test::Unit::TestCase
 
   def test_insertion_sort_two_numbers_correct
     test_array = [2, 1]
-    assert_equal([1, 2], Algorithms::Sort.insertion_sort(test_array))
+    assert_equal([1, 2], Sort.insertion_sort(test_array))
   end
 
   def test_insertion_sort_multiple_numbers
     test_array = [3, 2, 1, 5, 4]
-    assert_equal([1, 2, 3, 4, 5], Algorithms::Sort.insertion_sort(test_array))
+    assert_equal([1, 2, 3, 4, 5], Sort.insertion_sort(test_array))
   end
 
 end
