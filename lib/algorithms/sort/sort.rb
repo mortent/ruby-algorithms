97aa4107328fb5bfc628a41c35a518fa08ba6b2e
mortent <morten.j.torkildsen@gmail.com>
2013-08-01 22:06:15 -0400

    Implemented countingsort

--- lib/algorithms/sort/sort.rb
+++ lib/algorithms/sort/sort.rb
@@ -1,5 +1,24 @@
 module Algorithms::Sort
 
+  # Algorithm: Countingsort
+  # Requirements:
+  # Time complexity: O(n)
+  # Space complexity:
+  # Stable: Yes
+  #
+  # Algorithms::Sort.counting_sort([..]) => [..]
+  def self.counting_sort(array, k)
+    c = Array.new(k + 1, 0)
+    b = Array.new(array.length)
+    array.each {|element| c[element] += 1}
+    (0..k).inject(-1) {|sum, i| c[i] = sum + c[i]}
+    array.reverse.each do |element|
+      b[c[element]] = element
+      c[element] -= 1
+    end
+    b
+  end
+
 
   # Algorithm: Randomized quicksort
   # Requirements:
---
+++ test/algorithms/sort/test_counting_sort.rb
@@ -0,0 +1,24 @@
+require 'test/unit'
+require_relative '../../../lib/algorithms'
+
+class TestCountingSort < Test::Unit::TestCase
+
+  def test_should_correctly_sort_one_element_array
+    assert_equal([1], Algorithms::Sort.counting_sort([1], 1))
+  end
+
+  def test_should_correctly_sort_two_element_array
+    assert_equal([0, 2], Algorithms::Sort.counting_sort([0, 2], 3))
+  end
+
+  def test_should_correctly_sort_an_odd_number_of_elements
+    array = [3,5,2,4,1]
+    assert_equal(array.sort, Algorithms::Sort.counting_sort(array, 10))
+  end
+
+  def test_should_correctly_sort_an_even_number_of_elements
+    array = [3, 5, 2, 4, 1, 10, 0, 50]
+    assert_equal(array.sort, Algorithms::Sort.counting_sort(array, 50))
+  end
+
+end
